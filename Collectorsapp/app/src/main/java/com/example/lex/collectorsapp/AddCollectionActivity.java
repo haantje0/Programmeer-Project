aa5fe5f7fb26bb828a02a4cb783cb01b400b3636
haantje0 <l.d.haan@outlook.com>
2018-01-20 18:06:00 +0100

    Made the view of the new specs with a nice popup

--- Collectorsapp/app/src/main/java/com/example/lex/collectorsapp/AddCollectionActivity.java
+++ Collectorsapp/app/src/main/java/com/example/lex/collectorsapp/AddCollectionActivity.java
@@ -1,19 +1,27 @@
 package com.example.lex.collectorsapp;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AlertDialogLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import static com.example.lex.collectorsapp.R.id.container;
 
 public class AddCollectionActivity extends AppCompatActivity {
 
@@ -23,8 +31,6 @@ public class AddCollectionActivity extends AppCompatActivity {
     EditText editTextTitle;
 
     String title;
-
-    Integer id = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,48 +43,54 @@ public class AddCollectionActivity extends AppCompatActivity {
         dbManager.setDatabase();
     }
 
-    public void setTitle() {
-        editTextTitle = (EditText) findViewById(R.id.editTextTitle);
-        title = editTextTitle.getText().toString();
-    }
+    public void AddSpecs(View arg0) {
 
-    public void AddSpecs(View view) {
-        LinearLayout linearLayout = findViewById(R.id.LinearLayoutExtraSpecs);
+        AlertDialog.Builder builder = new AlertDialog.Builder(AddCollectionActivity.this);
+        builder.setTitle("Extra Specification:");
 
-        //added LayoutParams
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        LayoutInflater inflater = AddCollectionActivity.this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.spec_dialog, null);
+        builder.setView(dialogView);
 
-        //add textView
-        TextView textView = new TextView(this);
-        textView.setText("Spec Name");
-        textView.setLayoutParams(params);
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // set new spec
+                TextView textView = dialogView.findViewById(R.id.editTextNewSpec);
+                String newSpec = textView.getText().toString();
 
-        // added Button
-        Button button = new Button(this);
-        button.setText("Delete");
-        button.setTextColor(Color.RED);
-        button.setGravity(Gravity.RIGHT);
-        button.setLayoutParams(params);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                LinearLayout LLH = findViewById(v.getId());
-                LLH.setVisibility(View.GONE);
+                // put new spec in view
+                LinearLayout linearLayout = (LinearLayout) findViewById(R.id.LinearLayoutExtraSpecs);
+
+                LayoutInflater layoutInflater =
+                        (LayoutInflater) getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View addView = layoutInflater.inflate(R.layout.spec_row, null);
+                TextView textOut = (TextView)addView.findViewById(R.id.textout);
+                textOut.setText(newSpec);
+                TextView buttonRemove = (TextView) addView.findViewById(R.id.remove);
+                buttonRemove.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        ((LinearLayout)addView.getParent()).removeView(addView);
+                    }});
+
+                linearLayout.addView(addView);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
             }
         });
 
-        LinearLayout linearLayoutH = new LinearLayout(this);
-        linearLayoutH.setId(id);
-        linearLayoutH.setLayoutParams(params);
-        linearLayoutH.setOrientation(LinearLayout.HORIZONTAL);
+        AlertDialog dialog = builder.create();
+        dialog.show();
 
-        //added the textView and the Button to LinearLayout
-        linearLayout.addView(linearLayoutH);
 
-        linearLayoutH.addView(textView);
-        linearLayoutH.addView(button);
+    };
 
-        id += 1;
+    public void setTitle() {
+        editTextTitle = (EditText) findViewById(R.id.editTextTitle);
+        title = editTextTitle.getText().toString();
     }
 
     public void SaveCollection(View view) {
